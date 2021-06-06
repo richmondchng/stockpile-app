24286e01b5783690c657e3d3de25d9ae1dd147c1
richmondchng <richmondchng@gmail.com>
2021-06-06 18:13:20 +0800

    add data import component, initial impl to include simple form and inputs to upload to endpoint

--- stockpile-web/src/App.js
+++ stockpile-web/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import DataImport from './components/DataImport';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <h2>Hello world</h2>
+            <DataImport schema='articles' />
+        </div>
+    );
 }
 
 export default App;
