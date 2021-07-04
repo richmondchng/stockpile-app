38b92ea879123eb5f0bd497c0df4f52b425e65ff
richmondchng <richmondchng@gmail.com>
2021-07-04 22:23:18 +0800

    add upload schema config table

--- stockpile-web/src/components/pages/admin/UploadSchemas.js
+++ stockpile-web/src/components/pages/admin/UploadSchemas.js
@@ -1,8 +1,48 @@
+import { useState, useEffect } from 'react'
+import PageTitle from '../../common/PageTitle';
 
 const UploadSchemas = () => {
+
+    const [uploadSchemas, setUploadSchemas] = useState([]);
+
+    useEffect(() => {
+        const getUploadSchemas = async () => {
+            try {
+                const response = await fetch('/api/v1.0/import/', { method: 'GET'});
+                if(response.status === 200) {
+                    const jsonData = await response.json();
+                    setUploadSchemas(jsonData.data);
+                }
+            } catch(err) {
+                console.error(err);
+            }
+        };
+        getUploadSchemas();
+    }, []);
+
     return (
         <div>
-            hello
+            <PageTitle>Configure Upload Schemas</PageTitle>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Schema</th>
+                        <th scope="col">Topic</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {uploadSchemas.map((value) => {
+                        return (
+                        <tr>
+                            <td>{value.name}</td>
+                            <td>{value.schema}</td>
+                            <td>{value.topic}</td>
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 }
