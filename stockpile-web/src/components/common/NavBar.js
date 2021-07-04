ed81a25f5071dfef3a6b9876eb166b7f4369a1a8
richmondchng <richmondchng@gmail.com>
2021-07-04 18:12:15 +0800

    add nested nav item

--- stockpile-web/src/components/common/NavBar.js
+++ stockpile-web/src/components/common/NavBar.js
@@ -5,7 +5,7 @@ import NavBarItem from './NavBarItem';
  */
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-sm sticky-top navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">Stockpile</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,21 +13,13 @@ const NavBar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav">
-                        <NavBarItem label="Home" link="/" active="true"></NavBarItem>
-                        <NavBarItem label="Article" link="/articles"></NavBarItem>
-                        <NavBarItem label="Upload" link="/data-import"></NavBarItem>
-                        <NavBarItem label="About" link="/about"></NavBarItem>
-
-                        {/* <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li> */}
+                        <NavBarItem id="navHome" label="Home" link="/" active="true"></NavBarItem>
+                        <NavBarItem id="navArticle" label="Article" link="/articles"></NavBarItem>
+                        <NavBarItem id="navUpload" label="Upload" link="/data-import"></NavBarItem>
+                        <NavBarItem id="navAbout" label="About" link="#">
+                            <NavBarItem id="navConfigureUploadSchema" label="Configure upload schema" link="#" />
+                            <NavBarItem id="navAboutUs" label="About" link="/about" />
+                        </NavBarItem>
                     </ul>
                 </div>
             </div>
--- stockpile-web/src/components/common/NavBarItem.js
+++ stockpile-web/src/components/common/NavBarItem.js
@@ -2,19 +2,35 @@ import { Link } from 'react-router-dom';
 
 /**
  * Single navigation item.
+ * id = menu Id
  * label = Menu item label
  * link = Link to page, or # by default
  * active = If it is the current active page, false by default
  */
-const NavBarItem = ({label, link = "#", active = false}) => {
-    const activeClass = active ? 'nav-link active' : 'nav-link ';
-    const ariaCurrent = active ? {"aria-current":"page"} : {};
+const NavBarItem = ({id, label, link = "#", active = false, children}) => {
+    const listItemClass = children ? 'nav-item dropdown' : 'nav-time';
+
+    let menuLink
+    if(!children) {
+        // single level menu item
+        const activeClass = active ? 'nav-link active' : 'nav-link ';
+        const ariaCurrent = active ? {"aria-current":"page"} : {};
+        menuLink = <Link id={id} className={activeClass} {... ariaCurrent} to={link}>{label}</Link>;
+    } else {
+        // has sub-level menu item
+        menuLink = <Link id={id} to={link} className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">{label}</Link>
+    }
+
+    let menuChildList
+    if(children) {
+        // add list to build child nav item
+        menuChildList = <ul className="dropdown-menu bg-dark" aria-labelledby={id}>{children}</ul>;
+    }
 
     return (
-        <li className="nav-item">
-            <Link className={activeClass} {... ariaCurrent} to={link}>{label}</Link>
-            {/* <a className={activeClass} {... ariaCurrent} 
-                href={link}>{label}</a> */}
+        <li className={listItemClass}>
+            {menuLink}
+            {menuChildList}
         </li>
     )
 }
